40a8e74e586554f66692748e430ec14fb79450fb
Kohei Ichikawa <kohei.ichikawa@gmail.com>
2020-06-26 21:25:23 +0900

    Add some outputs

--- inonius-headless.py
+++ inonius-headless.py
@@ -12,18 +12,22 @@ options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 
 try:
+    print('Access to inonius.net')
     driver.get('https://inonius.net/')
     time.sleep(2)
     
+    print('Start to measure')
     button = driver.find_element_by_id('startTestBtn')
     button.click()
 
+    print('Wait 40 seconds for the result...')
     time.sleep(40)
 
-    w = driver.execute_script("return document.body.scrollWidth;")
-    h = driver.execute_script("return document.body.scrollHeight;")
+    w = driver.execute_script('return document.body.scrollWidth;')
+    h = driver.execute_script('return document.body.scrollHeight;')
     driver.set_window_size(w,h)
     driver.save_screenshot('result.png')
+    print('The result was saved as result.png')
 except:
     traceback.print_exc()
 finally:
